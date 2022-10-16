0f93fec2c99c5a343acf8e334adbdc437cfd790d
tasuwo <kamuhata.you@gmail.com>
2022-10-16 23:37:43 +0900

    :arrow_up: SwiftLint定義を更新

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/tasuwo/swift", from: "0.2.0"),
+        .package(url: "https://github.com/tasuwo/swift", from: "0.3.0"),
         .package(url: "https://github.com/apple/swift-async-algorithms", .upToNextMinor(from: "0.0.3"))
     ],
     targets: [
