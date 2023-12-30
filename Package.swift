442f67fceabae8e4486403a117b7b748da2304b3
tasuwo <kamuhata.you@gmail.com>
2023-12-30 15:28:57 +0900

    :arrow_up: サポートバージョンの更新

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "PersistentStack",
-    platforms: [.iOS(.v14), .macOS(.v14)],
+    platforms: [.iOS(.v15), .macOS(.v14)],
     products: [
         .library(
             name: "PersistentStack",
